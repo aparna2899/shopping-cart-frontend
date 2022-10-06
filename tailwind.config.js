91a624353d676ac0d377ed355cbaea6ae78684fd
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-10-06 21:10:51 +0530

    create sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/componenets/Products.js'],
+  content: [
+    './src/componenets/Products.js',
+    './src/componenets/Sizes.js',
+    './src/pages/Home.js',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
