18b7580de23a968242a6c383c15819a6bdb964c7
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-10-06 22:57:45 +0530

    add functionality to size filter

--- src/componenets/Sizes.js
+++ src/componenets/Sizes.js
@@ -1,11 +1,21 @@
-export default function Sizes() {
+export default function Sizes(props) {
+  const { selectedSize, handleClick } = props;
   const sizes = ['XS', 'S', 'M', 'X', 'L', 'XL', 'XXL'];
+
   return (
     <div className="mt-28">
       <h2 className="text-lg font-medium px-6 mb-2">Sizes:</h2>
       <ul className="grid grid-cols-3 px-4">
         {sizes.map((size) => (
-          <li key={size} className="w-10 h-10 rounded-full bg-gray-200 m-2 flex justify-center items-center cursor-pointer hover:bg-gray-300">
+          <li
+            key={size}
+            className={
+              selectedSize.includes(size)
+                ? 'w-10 h-10 rounded-full bg-black text-white m-2 flex justify-center items-center cursor-pointer'
+                : 'w-10 h-10 rounded-full bg-gray-200 m-2 flex justify-center items-center cursor-pointer hover:bg-gray-300'
+            }
+            onClick={() => handleClick(size)}
+          >
             <span className="text-sm">{size}</span>
           </li>
         ))}
@@ -13,4 +23,3 @@ export default function Sizes() {
     </div>
   );
 }
-
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import Products from '../componenets/Products';
 import Sizes from '../componenets/Sizes';
 
 export default function Home() {
+  const [selectedSize, setSelectedSize] = useState([]);
+  const handleClick = (size) => {
+    let arr = [...selectedSize];
+    if (arr.includes(size)) {
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === size) {
+          arr.splice(i, 1);
+          setSelectedSize(arr);
+        }
+      }
+    } else {
+      arr.push(size);
+      setSelectedSize(arr);
+    }
+  };
   return (
     <div className="mx-auto max-w-8xl sm:px-6 lg:px-8">
       <div className="grid grid-cols-6 gap-4">
-        <Sizes />
-        <Products />
+        <Sizes selectedSize={selectedSize} handleClick={handleClick} />
+        <Products selectedSize={selectedSize}/>
       </div>
     </div>
   );
