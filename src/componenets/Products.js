a9dd12e7d7e2f65cded3f6d3be4c90fcc818f84d
Aparna Tiwari <73102734+aparna2899@users.noreply.github.com>
2022-10-06 23:40:32 +0530

    add sorting

--- src/componenets/Products.js
+++ src/componenets/Products.js
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import data from '../data.json';
 
 export default function Products(props) {
+  const [sort, setSort] = useState('asc');
   let { selectedSize } = props;
   let productList = [];
   if (selectedSize.length === 0) {
@@ -17,9 +19,37 @@ export default function Products(props) {
       }
     }
   }
+  if (sort === 'asc') {
+    productList.sort((a, b) => a.price - b.price);
+  } else {
+    productList.sort((a, b) => b.price - a.price);
+  }
+
+  const handleChange = (event) => {
+    if (event.target.value === 'desc') {
+      setSort('desc');
+    } else {
+      setSort('asc');
+    }
+  };
 
   return (
     <div className="col-span-5 bg-white">
+      <div className="absolute right-16 top-8">
+        <label htmlFor="order">Order by</label>
+        <select
+          id="order"
+          defaultValue={'DEFAULT'}
+          className="rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 mx-4 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+          onChange={(event) => handleChange(event)}
+        >
+          <option value="DEFAULT" disabled>
+            Sort
+          </option>
+          <option value="asc">Lowest to highest</option>
+          <option value="desc">Highest to lowest</option>
+        </select>
+      </div>
       <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <ul className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
           {productList.map((product) => (
@@ -39,7 +69,9 @@ export default function Products(props) {
                   <p className="mt-1 text-sm text-gray-500">{product.style}</p>
                   <p className="mt-1 text-sm text-gray-500 inline">Sizes </p>
                   {product.availableSizes.map((size) => (
-                    <span className="mt-1 text-sm text-gray-500">| {size} </span>
+                    <span key={size} className="mt-1 text-sm text-gray-500">
+                      | {size}{' '}
+                    </span>
                   ))}
                 </div>
                 <div className="absolute inset-x-0 top-0 flex h-96 items-end justify-end overflow-hidden rounded-lg p-4">
